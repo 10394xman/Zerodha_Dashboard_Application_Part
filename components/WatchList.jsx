65f066a30ef5a84dbe398e6d0f0efde9b02c5b00
Xman <xman10394@gmail.com>
2025-08-17 17:09:30 +0530

    WatchList section created

--- components/WatchList.jsx
+++ components/WatchList.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { Tooltip, Grow } from "@mui/material";
 
+import { watchlist } from "../data/data";
 const WatchList = () => {
   return (
     <div className="watchlist-container">
@@ -11,12 +13,82 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts"> {watchlist.length} / 50</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+        {watchlist.map((stock, index) => {
+          return <WatchListItem stock={stock} key={index} />;
+        })}
+      </ul>
     </div>
   );
 };
 
 export default WatchList;
+
+//WatchListItem is not reused so we are going to create this comp. below
+const WatchListItem = ({ stock }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="item">
+        <p>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (
+            <span className="material-symbols-outlined down">south</span>
+          ) : (
+            <span className="material-symbols-outlined up">
+              arrow_upward_alt
+            </span>
+          )}
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+      {isHovered && <WatchListActions uid={stock.name} />}
+    </li>
+  );
+};
+
+const WatchListActions = (uid) => {
+  return (
+    <span className="actions">
+      <Tooltip title="Buy (B)" placement="top" arrow TransitionComponent={Grow}>
+        <button className="buy">Buy</button>
+      </Tooltip>
+      <Tooltip
+        title="Sell (S)"
+        placement="top"
+        arrow
+        TransitionComponent={Grow}
+      >
+        <button className="sell">Sell</button>
+      </Tooltip>
+      <Tooltip
+        title="Analytics (A)"
+        placement="top"
+        arrow
+        TransitionComponent={Grow}
+      >
+        <button className="action">
+          <span class="material-symbols-outlined">bar_chart</span>
+        </button>
+      </Tooltip>
+      <Tooltip title="More" placement="top" arrow TransitionComponent={Grow}>
+        <button className="action">
+          <span class="material-symbols-outlined">more_horiz</span>
+        </button>
+      </Tooltip>
+    </span>
+  );
+};
